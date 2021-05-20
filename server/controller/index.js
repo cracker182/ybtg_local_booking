01f7876be7ea195e5002ce8325fe42746b6a425e
Marco May <git@creaser.de>
2021-05-20 19:48:29 +0200

    basic auth added

--- server/controller/index.js
+++ server/controller/index.js
@@ -8,6 +8,8 @@ const EXPRESS = require('express');
 const BODYPARSER = require('body-parser');
 const MORGAN = require('morgan');
 
+const userController_o = require('./user_c');
+
 let app_o = EXPRESS();
 
 let baseDir_a = PATH.dirname(require.main.filename).split(PATH.sep);
@@ -18,17 +20,12 @@ app_o.use(EXPRESS.static(PATH.join(baseDir_s, 'client')));
 
 
 
-
 app_o.get('/', function (req, res) {
-
     //let path_s = PATH.join(baseDir_s, 'client', 'html', 'test.html');
-
-    res.sendFile(path_s)
+    //res.sendFile(path_s)
 });
 
-
-
-
+app_o.use('/', userController_o);
 
 
 module.exports = app_o;
