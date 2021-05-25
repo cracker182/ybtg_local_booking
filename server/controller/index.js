5f808271605ad1eb608d7b9c9eb5653474d77ac3
Marco May <git@creaser.de>
2021-05-25 19:42:15 +0200

    pug added

--- server/controller/index.js
+++ server/controller/index.js
@@ -8,6 +8,8 @@ const EXPRESS = require('express');
 const BODYPARSER = require('body-parser');
 const MORGAN = require('morgan');
 
+const db_user = require("../db/user_db");
+
 const userController_o = require('./user_c');
 const eventController_o = require('./event_c');
 const bookingController_o = require('./booking_c');
@@ -19,17 +21,17 @@ baseDir_a.pop();
 let baseDir_s = baseDir_a.join(PATH.sep);
 
 app_o.use(EXPRESS.static(PATH.join(baseDir_s, 'client')));
-
-
-
-app_o.get('/', function (req, res) {
-    //let path_s = PATH.join(baseDir_s, 'client', 'html', 'test.html');
-    //res.sendFile(path_s)
-});
+app_o.set('view engine', 'pug');
 
 app_o.use('/', userController_o);
 app_o.use('/', eventController_o);
 app_o.use('/', bookingController_o);
 
+
+app_o.get('/', async function (req, res) {
+    const users = await db_user.getAllUsers();
+    console.log(users);
+    res.render('main', { userList: users });
+});
 
 module.exports = app_o;
